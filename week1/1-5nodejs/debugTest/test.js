6e91c4b8cf44198a5f39a4b46a48dcec4a79d522
Min-92 <okssud1@naver.com>
2019-04-05 18:26:16 +0900

    call stack학습을 위한 코드 작성
    디버깅 call stack 학습을 위한 코드 작성

---
+++ week1/1-5nodejs/debugTest/test.js
@@ -0,0 +1,57 @@
+
+for(var i = 0; true; i++){
+
+    console.log(`Hello world ${i}`);
+
+} 
+
+
+
+// function1();
+
+// function function1(){
+//     function2()
+// }
+// function function2(){
+//     function3()
+// }
+// function function3(){
+//     function4()
+// }
+// function function4(){
+//     function5()
+// }
+// function function5(){
+//     function6()
+// }
+// function function6(){
+//     function7()
+// }
+// function function7(){
+//     function8()
+// }
+// function function8(){
+//     function9()
+// }
+// function function9(){
+//     function10()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
