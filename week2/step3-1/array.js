d7f919310fd3630b5aa33c7bf5c61a63f0316819
Min-92 <okssud1@naver.com>
2019-04-15 22:04:22 +0900

    step 3-1 미션 1 배열만들기
    데이터에서 숫자타입으로 된 구성요소만 뽑아서 배열 만들기

---
+++ week2/step3-1/array.js
@@ -0,0 +1,41 @@
+const data = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": {
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}
+
+let result = [];
+
+const dataValues = Object.values(data);
+
+dataValues.forEach(function (element) {
+    const entries = Object.entries(element);
+    entries.forEach(function(element){
+        if(typeof (element[1]) === 'number') {
+            result.push(element[0]);
+        }
+    });
+});
+
+console.log(result);    
