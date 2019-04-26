28f026d56c090d55c33542cee5195a0446b062df
Min-92 <okssud1@naver.com>
2019-04-27 00:03:00 +0900

    feat : complite step5-2
    - complite mission with mukeunzi

--- week4/step5-2/app.js
+++ week4/step5-2/app.js
@@ -1,16 +1,10 @@
 const todo = module.require('./todo.js');
-// 입력받는 함수
-// 프롬프트 사용
-// 데이터 파싱
-// $ 없는경우 에러처리
-// 파싱 불가능한 경우 에러처리
-// 명령호출을 호출
-// const inputCommand = () => {
 const readline = require('readline');
+const errorMessage = module.require('./errorMessage.js');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 const myTodo = new todo(rl);
 
@@ -18,28 +12,28 @@ rl.setPrompt('명령하세요 : ');
 
 rl.prompt();
 rl.on('line', input => {
-    const inputArray = input.split('$');
-    if (inputArray.length === 1) {
-        return myTodo.printError('COMMAND_ERROR');
-    }
-    const action = inputArray.splice(0, 1)[0];
-    const condition = inputArray;
+	const inputArray = input.split('$');
+	if (inputArray.length === 1) {
+		return myTodo.printError('COMMAND_ERROR');
+	}
+	const action = inputArray.splice(0, 1)[0];
+	const condition = inputArray;
 
-    excuteTodo(action, condition, rl);
+	excuteTodo(action, condition);
 });
-// };
-// 명령어 호출해주는 함수
-// 입력받은 명령을 호출
 
 const excuteTodo = (action, condition) => {
-    const regExp = /^show$|^add$|^delete$|^update$/;
-    const matchRegExp = action.match(regExp);
-    if (matchRegExp === null) {
-        myTodo.printError('COMMAND_ERROR');
-    } else {
-        myTodo[action](...condition);
-    }
-    // readline.prompt();
-};
+	const regExp = /^show$|^add$|^delete$|^update$/;
+	const matchRegExp = action.match(regExp);
 
-// inputCommand();
+	try {
+		if (matchRegExp === null) {
+			throw new Error(errorMessage.COMMAND_ERROR);
+		} else {
+			myTodo[action](...condition);
+		}
+	} catch (e) {
+		console.log(e.message);
+		rl.prompt();
+	}
+};
--- week4/step5-2/todo.js
+++ week4/step5-2/todo.js
@@ -3,134 +3,108 @@ const errorMessage = module.require('./errorMessage.js');
 module.require('date-utils');
 
 module.exports = class todo {
-    constructor(rl) {
-        this.readline = rl;
-    }
-    show(element) {
-        const regExp = /^all$|^todo$|^doing$|^done$/;
-        const matchRegExp = element.match(regExp);
-        if (matchRegExp === null) {
-            this.printError('COMMAND_ERROR');
-        } else if (matchRegExp[0] === 'all') {
-            this.printAll();
-        } else {
-            this.printList(matchRegExp[0]);
-        }
-    }
+	constructor(rl) {
+		this.readline = rl;
+	}
+	show(element) {
+		const regExp = /^all$|^todo$|^doing$|^done$/;
+		const matchRegExp = element.match(regExp);
+		if (matchRegExp === null) {
+			throw new Error(errorMessage.COMMAND_ERROR);
+		} else if (matchRegExp[0] === 'all') {
+			this.printAll();
+		} else {
+			this.printList(matchRegExp[0]);
+		}
+	}
 
-    getStatusList() {
-        const listOfStatus = todoList.reduce((acc, cur) => {
-            if (acc[cur.status] === undefined) {
-                acc[cur.status] = [cur.name];
-            } else {
-                acc[cur.status].push(cur.name);
-            }
-            return acc;
-        }, {});
-        return listOfStatus;
-    }
-    printAll() {
-        const listOfStatus = this.getStatusList();
-        let printResult = [];
-        for (let status in listOfStatus) {
-            printResult.push(`${status} : ${listOfStatus[status].length}개`);
-        }
-        console.log(`현재상태 : ${printResult.join(', ')}`);
-        this.readline.prompt();
-    }
+	getStatusList() {
+		const listOfStatus = todoList.reduce((acc, cur) => {
+			if (acc[cur.status] === undefined) {
+				acc[cur.status] = [cur.name];
+			} else {
+				acc[cur.status].push(cur.name);
+			}
+			return acc;
+		}, {});
+		return listOfStatus;
+	}
+	printAll() {
+		const listOfStatus = this.getStatusList();
+		let printResult = [];
+		for (let status in listOfStatus) {
+			printResult.push(`${status} : ${listOfStatus[status].length}개`);
+		}
+		console.log(`현재상태 : ${printResult.join(', ')}`);
+		this.readline.prompt();
+	}
 
-    printList(status) {
-        const listOfStatus = this.getStatusList();
-        const statusList = listOfStatus[`${status}`];
-        let outputString = `${status}리스트 : 총 ${statusList.length} 건 : `;
-        for (let i in statusList) {
-            outputString += statusList[i];
-            if (i < statusList.length - 1) {
-                outputString += ', ';
-            }
-        }
-        console.log(outputString);
-        this.readline.prompt();
-    }
+	printList(status) {
+		const listOfStatus = this.getStatusList();
+		const statusList = listOfStatus[status];
+		console.log(`${status}리스트 : 총 ${statusList.length} 건 : ${statusList.join(', ')}`);
+		this.readline.prompt();
+	}
 
-    add(name, tag) {
-        console.log(tag + '-----');
-        const regExp = /^\[\'.*\'\]$/;
-        const matchRegExp = tag.match(regExp);
-        if (matchRegExp === null) {
-            return this.printError('TAG_ERROR');
-        }
-        const tagResult = tag.replace(/\[|\'|\]/g, '').split(',');
-        const id = this.getId();
-        const newData = {
-            name: name,
-            tags: tagResult,
-            status: 'todo',
-            id: Number(id)
-        };
-        todoList.push(newData);
-        console.log(todoList);
-        setTimeout(() => {
-            this.printAll();
-        }, 1000);
-    }
+	add(name, tag) {
+		const tagResult = tag.replace(/\[|\'|\"|\]/g, '').split(',');
+		const id = this.getId();
+		const newData = {
+			name: name,
+			tags: tagResult,
+			status: 'todo',
+			id: id
+		};
+		todoList.push(newData);
+		console.log(`${newData.name} 1개가 추가되었습니다. (id : ${newData.id})`);
+		setTimeout(() => {
+			this.printAll();
+		}, 1000);
+	}
 
-    getId() {
-        const id = new Date();
-        return id.toFormat('YYMMDDHHMISS');
-    }
+	getId() {
+		const id = new Date();
+		return Number(id.toFormat('YYMMDDHHMISS'));
+	}
 
-    findData(id) {
-        let index;
-        const targetData = todoList.filter((element, innerIndex) => {
-            if (Number(id) === element.id) {
-                index = innerIndex;
-                return Number(id) === element.id;
-            }
-        });
+	checkValidId(id) {
+		let index;
+		const targetData = todoList.filter((element, innerIndex) => {
+			if (Number(id) === element.id) {
+				index = innerIndex;
+				return Number(id) === element.id;
+			}
+		});
+		if (targetData[0] === undefined) {
+			throw new Error(errorMessage.ID_ERROR);
+		}
 
-        return [targetData[0], index];
-    }
+		return index;
+	}
 
-    delete(id) {
-        const deletingData = this.findData(id);
-        if (deletingData[0] === undefined) {
-            return this.printError('ID_ERROR');
-        }
-        const index = this.findData(id)[1];
-        const deletingName = deletingData[0].name;
-        todoList.splice(index, 1);
+	delete(id) {
+		const index = this.checkValidId(id);
+		const deletingName = todoList[index].name;
 
-        console.log(`${deletingName}가 ${deletingData[0].status}에서 삭제됐습니다.`);
-        setTimeout(() => {
-            this.printAll();
-        }, 1000);
-    }
+		console.log(`${deletingName}가 ${todoList[index].status}에서 삭제됐습니다.`);
+		todoList.splice(index, 1);
+		setTimeout(() => {
+			this.printAll();
+		}, 1000);
+	}
 
-    update(id, status) {
-        const targetData = this.findData(id)[0];
-        const index = this.findData(id)[1];
+	update(id, status) {
+		const index = this.checkValidId(id);
+		if (todoList[index].status === status) {
+			throw new Error(errorMessage.STATUS_ERROR);
+		}
+		todoList[index].status = status;
 
-        if (targetData === undefined) {
-            return this.printError('ID_ERROR');
-        }
-
-        if (todoList[index].status === status) {
-            return this.printError('STATUS_ERROR');
-        }
-
-        todoList[index].status = status;
-
-        setTimeout(() => {
-            console.log(`"${targetData.name}"가(이) ${status}로 변경되었습니다.`);
-            setTimeout(() => {
-                this.printAll();
-            }, 1000);
-        }, 3000);
-    }
-
-    printError(error) {
-        console.log(errorMessage[error]);
-        this.readline.prompt();
-    }
+		setTimeout(() => {
+			console.log(`"${todoList[index].name}"가(이) ${status}로 변경되었습니다.`);
+			setTimeout(() => {
+				this.printAll();
+			}, 1000);
+		}, 3000);
+	}
 };
