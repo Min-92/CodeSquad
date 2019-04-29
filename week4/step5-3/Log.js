8f4623c0ffb4a1c61aea0cce798d80a0cda94b80
Min-92 <okssud1@naver.com>
2019-04-29 10:15:57 +0900

    refactor : modify log.js to portotype
    - log.js 프로토타입으로 변경

--- week4/step5-3/Log.js
+++ week4/step5-3/Log.js
@@ -1,77 +1,146 @@
-const todoList = module.require('./data.js');
-module.exports = class Log {
-	constructor() {
-		(this.queue = []), (this.index = -1);
-	}
+require('date-utils');
+const Log = module.require('./log.js');
+const todoLog = new Log();
 
-	addLog(action, prevData, nextData, todoListIndex) {
-		if (this.queue.length > 4) {
-			this.queue.shift();
-		}
+const commonDelaySecond = 1000;
+const updateDelaySecond = 3000;
 
-		while (this.queue.length > this.index + 1) {
-			this.queue.pop();
-		}
+module.exports = Todo = function(todoList, rl) {
+    this.todoList = todoList;
+    this.readline = rl;
+};
 
-		this.queue[++this.index] = {
-			action: action,
-			prevData: prevData,
-			nextData: nextData,
-			todoListIndex: todoListIndex
-		};
-	}
+Todo.prototype.show = function(element) {
+    const regExp = /^all$|^todo$|^doing$|^done$/;
+    const matchRegExp = element.match(regExp);
+    if (matchRegExp === null) {
+        throw new Error('COMMAND_ERROR');
+    } else if (matchRegExp[0] === 'all') {
+        this.printAll();
+    } else {
+        this.printList(matchRegExp[0]);
+    }
+};
 
-	undo() {
-		if (this.index < 0) {
-			throw new Error('UNDO_ERROR');
-		}
+Todo.prototype.getStatusList = function() {
+    const listOfStatus = this.todoList.reduce((acc, cur) => {
+        if (acc[cur.status] === undefined) {
+            acc[cur.status] = [cur.name];
+        } else {
+            acc[cur.status].push(cur.name);
+        }
+        return acc;
+    }, {});
+    return listOfStatus;
+};
 
-		const action = this.queue[this.index].action;
-		const prevData = this.queue[this.index].prevData;
-		const nextData = this.queue[this.index].nextData;
-		const todoListIndex = this.queue[this.index].todoListIndex;
+Todo.prototype.printAll = function() {
+    const listOfStatus = this.getStatusList();
+    let printResult = [];
+    for (let status in listOfStatus) {
+        printResult.push(`${status} : ${listOfStatus[status].length}개`);
+    }
+    console.log(`현재상태 : ${printResult.join(', ')}`);
+    this.readline.prompt();
+};
 
-		if (action === 'add') {
-			this.alterData(todoListIndex, 1);
-		} else if (action === 'delete') {
-			this.alterData(todoListIndex, 0, prevData);
-		} else if (action === 'update') {
-			this.alterData(todoListIndex, 1, prevData);
-		}
-		console.log(
-			`"${nextData.id}"번 항목 '${nextData.name}'이(가) ${nextData.status} 에서 ${prevData.status}로 변경되었습니다.`
-		);
-		this.index--;
-	}
+Todo.prototype.printList = function(status) {
+    const listOfStatus = this.getStatusList();
+    const statusList = listOfStatus[status];
+    console.log(`${status}리스트 : 총 ${statusList.length} 건 : ${statusList.join(', ')}`);
+    this.readline.prompt();
+};
 
-	redo() {
-		if (this.index >= this.queue.length - 1) {
-			throw new Error('REDO_ERROR');
-		}
+Todo.prototype.add = function(name, tag) {
+    if (name === undefined || tag === undefined || name === '' || tag === '') {
+        throw new Error('COMMAND_ERROR');
+    }
+    const tagResult = tag.replace(/\[|\'|\"|\]/g, '').split(',');
+    const id = this.getId();
+    const newData = {
+        name: name,
+        tags: tagResult,
+        status: 'todo',
+        id: id
+    };
+    this.todoList.push(newData);
+    const prevData = {};
+    Object.assign(prevData, newData);
+    prevData.status = '삭제';
+    console.log(`${newData.name} 1개가 추가되었습니다. (id : ${newData.id})`);
+    todoLog.addLog('add', prevData, newData, this.todoList.length - 1);
+    setTimeout(() => {
+        this.printAll();
+    }, commonDelaySecond);
+};
 
-		this.index++;
-		const action = this.queue[this.index].action;
-		const prevData = this.queue[this.index].prevData;
-		const nextData = this.queue[this.index].nextData;
-		const todoListIndex = this.queue[this.index].todoListIndex;
+Todo.prototype.getId = function() {
+    const id = new Date();
+    return Number(id.toFormat('YYMMDDHHMISS'));
+};
 
-		if (action === 'add') {
-			this.alterData(todoListIndex, 0, nextData);
-		} else if (action === 'delete') {
-			this.alterData(todoListIndex, 1);
-		} else if (action === 'update') {
-			this.alterData(todoListIndex, 1, nextData);
-		}
+Todo.prototype.checkValidId = function(id) {
+    let index;
+    const targetData = this.todoList.filter((element, innerIndex) => {
+        if (Number(id) === element.id) {
+            index = innerIndex;
+            return Number(id) === element.id;
+        }
+    });
+    if (targetData[0] === undefined) {
+        throw new Error('ID_ERROR');
+    }
 
-		console.log(
-			`"${nextData.id}"번 항목 '${nextData.name}'이(가) ${prevData.status} 에서 ${nextData.status}로 변경되었습니다.`
-		);
-	}
-	alterData(todoListIndex, deleteCount, data) {
-		if(data === undefined){
-			todoList.splice(todoListIndex, deleteCount);
-		}else{
-			todoList.splice(todoListIndex, deleteCount, data);
-		}
-	}
+    return index;
+};
+
+Todo.prototype.delete = function(id) {
+    const index = this.checkValidId(id);
+    const deletingName = this.todoList[index].name;
+
+    console.log(`${deletingName}가 ${this.todoList[index].status}에서 삭제됐습니다.`);
+    const nextData = {};
+    Object.assign(nextData, this.todoList[index]);
+    nextData.status = '삭제';
+    todoLog.addLog('delete', this.todoList[index], nextData, index);
+    this.todoList.splice(index, 1);
+    setTimeout(() => {
+        this.printAll();
+    }, commonDelaySecond);
+};
+
+Todo.prototype.update = function(id, status) {
+    if (status === undefined) {
+        throw new Error('COMMAND_ERROR');
+    }
+    const index = this.checkValidId(id);
+    const regExp = /^todo$|^doing$|^done$/;
+    const matchRegExp = status.match(regExp);
+
+    if (matchRegExp === null) {
+        throw new Error('COMMAND_ERROR');
+    } else if (this.todoList[index].status === status) {
+        throw new Error('STATUS_ERROR');
+    }
+    const prevData = {};
+    Object.assign(prevData, this.todoList[index]);
+    this.todoList[index].status = status;
+    todoLog.addLog('update', prevData, this.todoList[index], index);
+
+    setTimeout(() => {
+        console.log(`"${this.todoList[index].name}"가(이) ${status}로 변경되었습니다.`);
+        setTimeout(() => {
+            this.printAll();
+        }, commonDelaySecond);
+    }, updateDelaySecond);
+};
+
+Todo.prototype.undo = function() {
+    todoLog.undo();
+    this.readline.prompt();
+};
+
+Todo.prototype.redo = function() {
+    todoLog.redo();
+    this.readline.prompt();
 };
